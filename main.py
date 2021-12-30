543a8262f8b2a880931ff3d5479b6f993c85920c
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-30 19:09:25 +0000

    Using Dictionary Comprehension to split a sentence into words and count the number of letters in each.

--- main.py
+++ main.py
@@ -4,11 +4,25 @@
 
 import random
 
-names = ['Jon', 'Bill', 'Maria', 'Jenny', 'Jack', 'Ruby', 'Python', 'Perl', 'January', 'February', 'March']
-print(f"Names: {names}")
+#
+# names = ['Jon', 'Bill', 'Maria', 'Jenny', 'Jack', 'Ruby', 'Python', 'Perl', 'January', 'February', 'March']
+# print(f"Names: {names}")
+#
+# student_score = {student: random.randint(0, 100) for student in names}  # student score
+# print(f"Student Score: {student_score}")
+#
+# passed_student = {student: score for (student, score) in student_score.items() if score > 60}  # passed students
+# print(f"Passed Students: {passed_student}")
 
-student_score = {student: random.randint(0, 100) for student in names}  # student score
-print(f"Student Score: {student_score}")
+# Instructions
+# You are going to use Dictionary Comprehension to create a dictionary called result that takes each word in the given
+# sentence and calculates the number of letters in each word.
+# Try Googling to find out how to convert a sentence into a list of words.
 
-passed_student = {student: score for (student, score) in student_score.items() if score > 60}  # passed students
-print(f"Passed Students: {passed_student}")
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+result = {word: len(word) for word in sentence.split()}
+
+print(result)
